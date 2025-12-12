f3e3e0adbad39ff5c16e79e77ac4a84b7527686b
DELONE-de <ibiyemiaina27@gmail.com>
2025-12-12 20:44:33 +0000

    Model script

--- terraform/orchestration/orchestration_files.tf
+++ terraform/orchestration/orchestration_files.tf
@@ -2,5 +2,5 @@ resource "aws_s3_object" "training_script" {
   bucket = var.s3_bucket_name
   key    = "scripts/train.py"
   source = "path/to/your/train.py"
-  etag   = filemd5("path/to/your/train.py")
+  etag   = filemd5("../Cloud-ETL/sagemaker/train.py")
 }
